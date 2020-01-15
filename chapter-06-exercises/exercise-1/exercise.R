d41314d58f406516d37bc90a5644f12797c6f0f4
Chengliang Zheng <JohnZheng@Chengliangs-MacBook-Pro.local>
2020-01-14 16:43:16 -0800

    chapter 5and6 exercise

--- chapter-06-exercises/exercise-1/exercise.R
+++ chapter-06-exercises/exercise-1/exercise.R
@@ -1,14 +1,14 @@
 # Exercise 1: calling built-in functions
 
 # Create a variable `my_name` that contains your name
-
+my_name <- "John"
 
 # Create a variable `name_length` that holds how many letters (including spaces)
 # are in your name (use the `nchar()` function)
-
+name_length <- nchar(my_name)
 
 # Print the number of letters in your name
-
+print(name_length)
 
 # Create a variable `now_doing` that is your name followed by "is programming!" 
 # (use the `paste()` function)
