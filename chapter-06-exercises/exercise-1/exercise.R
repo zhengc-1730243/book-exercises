f77a367a64dd2b7378697221ad7cf7c116b5c9ec
Chengliang Zheng <JohnZheng@Chengliangs-MacBook-Pro.local>
2020-01-16 20:55:49 -0800

    all required exercise

--- chapter-06-exercises/exercise-1/exercise.R
+++ chapter-06-exercises/exercise-1/exercise.R
@@ -12,35 +12,38 @@ print(name_length)
 
 # Create a variable `now_doing` that is your name followed by "is programming!" 
 # (use the `paste()` function)
-
+now_doing <- paste(my_name , "is programing!")
 
 # Make the `now_doing` variable upper case
-
+toupper(now_doing)
 
 ### Bonus
 
 # Pick two of your favorite numbers (between 1 and 100) and assign them to 
 # variables `fav_1` and `fav_2`
-
+fav_1 <- 7 
+fav_2 <- 11
 
 # Divide each number by the square root of 201 and save the new value in the
 # original variable
-
+fav_1 <- fav_1 / sqrt(201)
+fav_2 <- fav_2 / sqrt(201)
 
 # Create a variable `raw_sum` that is the sum of the two variables. Use the 
 # `sum()` function for practice.
-
+raw_sum <- sum(fav_1 , fav_2)
 
 # Create a variable `round_sum` that is the `raw_sum` rounded to 1 decimal place.
 # Use the `round()` function.
-
+round_sum <- round(raw_sum)
 
 # Create two new variables `round_1` and `round_2` that are your `fav_1` and 
 # `fav_2` variables rounded to 1 decimal places
-
+round_1 <- round(fav_1)
+round_2 <- round(fav_2)
 
 # Create a variable `sum_round` that is the sum of the rounded values
-
+sum_round <- sum(round_1 , round_2)
 
 # Which is bigger, `round_sum` or `sum_round`? (You can use the `max()` function!)
-
+max(round_sum, sum_round)
